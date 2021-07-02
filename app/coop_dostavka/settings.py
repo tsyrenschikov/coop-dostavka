e45fad754e4439e8f1b239a2a4d6746bad4b5098
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2021-07-02 16:56:18 +0500

    adminpanel

--- app/coop_dostavka/settings.py
+++ app/coop_dostavka/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'adminpanel.apps.AdminpanelConfig',
     'dashboard.apps.DashboardConfig',
     'grid.apps.GridConfig',
     'plug.apps.PlugConfig',

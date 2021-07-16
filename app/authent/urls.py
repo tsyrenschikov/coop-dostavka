2a7598d499c0fa1d175b980604b318ade4d7e467
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2021-07-16 19:58:14 +0500

    rename path dashboard signup

--- app/authent/urls.py
+++ app/authent/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from .views import reset_user
+from .views import reset_user, register
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -8,6 +8,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/',LogoutView.as_view(template_name='auth/login.html'), name='logout'),
-    path('signup/',views.SignUp, name='signup'),
+    path('signup/',register, name='signup'),
     path('resetpass/', reset_user, name='reset'),
 ]
--- app/authent/views.py
+++ app/authent/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import UserRegistrationForm
 
 
 def SignUp(reguest):
@@ -8,6 +8,22 @@ def SignUp(reguest):
 def reset_user(request):
     pass
     return render(request, "auth/resetpass.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'dashboard/dashboard.overview.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'auth/signup.html', {'user_form': user_form})
 
 
 

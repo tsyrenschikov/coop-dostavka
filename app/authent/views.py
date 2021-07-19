efcc6b9005db35c0749c74ba900dda5780a875d4
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2021-07-19 23:01:41 +0500

    valid client email and password

--- app/authent/views.py
+++ app/authent/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import  render, redirect
 from django.views.generic import TemplateView
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+
 
 def reset_user(request):
     pass
     return render(request, "auth/resetpass.html")
 
 class SignupView(TemplateView):
+    def __init__(self, min_length=6):
+        self.min_length = min_length
     template_name = "auth/signup.html"
 
     def dispatch(self, request, *args, **kwargs):
@@ -19,6 +24,12 @@ class SignupView(TemplateView):
             password = request.POST.get('password')
             password2 = request.POST.get('password2')
 
+            if len(password) < self.min_length:
+                raise ValidationError(
+                    _("This password must contain at least %(min_length)d characters."),
+                    code='password_too_short',
+                    params={'min_length': self.min_length},
+                )
             if password == password2:
                 User.objects.create_user(username=username,first_name=first_name,last_name=last_name, email=email, password=password)
                 return redirect(reverse("dashboard"))

7a13178514064a2b5df258586ac3a9bf86f82900
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2021-07-20 23:28:38 +0500

    signup

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import  render, redirect
-from django.views.generic import TemplateView
 from django.urls import reverse
 from django.contrib.auth.models import User
 
@@ -9,19 +8,28 @@ def reset_user(request):
     pass
     return render(request, "auth/resetpass.html")
 
-class SignupView(TemplateView):
-    template_name = "auth/signup.html"
+def signup(request):
 
-    def dispatch(self, request, *args, **kwargs):
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            first_name = request.POST.get('first_name')
-            email = request.POST.get('email')
-            password = request.POST.get('password')
-            password2 = request.POST.get('password2')
+    alert = {
+        "username": request.GET.get('username', ''),
+    }
 
-            if password == password2:
-                User.objects.create_user(username=username,first_name=first_name, email=email, password=password)
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        first_name = request.POST.get('first_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if password == password2:
+            if User.objects.filter(username=request.POST['username']).exists():
+                alert['username'] = "Имя пользователя занято"
+            if User.objects.filter(email=request.POST['email']).exists():
+                alert['email'] = "Почтовый ящик уже используется"
+            else:
+                User.objects.create_user(username=username, first_name=first_name, email=email, password=password)
                 return redirect(reverse("dashboard"))
 
-        return render(request, self.template_name)
+
+
+    return render(request, 'auth/signup.html', alert)

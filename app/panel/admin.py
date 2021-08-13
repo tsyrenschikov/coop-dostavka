1eb88d4a350578de8c5a7d8919f45e44d34dc4bb
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2021-08-13 14:28:00 +0500

    models category, subcategory,product

--- app/panel/admin.py
+++ app/panel/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Shop,Category,SubCategory, Product
 
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Shop, ShopAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug','number']
     prepopulated_fields = {'slug': ('name',)}
 admin.site.register(Category, CategoryAdmin)
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(SubCategory, SubCategoryAdmin)
+
 
 
 class ProductAdmin(admin.ModelAdmin):
--- app/panel/models.py
+++ app/panel/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
+class Shop(models.Model):
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Название магазина')
+    slug = models.SlugField(max_length=200,db_index=True, unique=True)
+    status = models.BooleanField(default=True, verbose_name='Статус активный')
+    descriptions = models.TextField(max_length=300, null=True, verbose_name='Описание магазина')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Магазин'
+        verbose_name_plural = 'Магазины'
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=True, verbose_name='Магазин')
     name = models.CharField(max_length=200, db_index=True, verbose_name='Имя')
     number = models.IntegerField(blank=True, null=True,verbose_name='Номер')
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
@@ -17,21 +31,36 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True, verbose_name='Категория')
+    name = models.CharField(max_length=25, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, db_index=True, null=True, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Подкатегория'
+        verbose_name = 'Подкатегория'
+        ordering = ['name']
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
-    name = models.CharField(max_length=200, db_index=True)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True , related_name='Подкатегория')
+    name = models.CharField(max_length=200, db_index=True,verbose_name='Название продукта')
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
+    description = models.TextField(blank=True,verbose_name='Описания продукта')
+    price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='Цена')
+    stock = models.PositiveIntegerField(verbose_name='Склад')
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('name',)
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
         index_together = (('id', 'slug'),)
 
     def __str__(self):

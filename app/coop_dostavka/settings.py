8b3e8fde24e35db5f60d8791ce7d9b46793cf9b2
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2021-09-15 14:41:50 +0500

    subcategory format

--- app/coop_dostavka/settings.py
+++ app/coop_dostavka/settings.py
@@ -163,7 +163,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-en'
 
 TIME_ZONE = 'Asia/Yekaterinburg'
 
--- app/panel/models.py
+++ app/panel/models.py
@@ -86,7 +86,7 @@ class Category(models.Model):
 class SubCategory(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True, verbose_name='Категория')
     name = models.CharField(max_length=200, unique=True, verbose_name='Название')
-    number = models.FloatField(blank=True, null=True,default=0.0, verbose_name='Номер')
+    number = models.FloatField(blank=True, null=True, verbose_name='Номер')
     image = models.ImageField(upload_to='subcategory/%Y/%m/%d', blank=True, null=True, verbose_name='Изображение')
 
     class Meta:

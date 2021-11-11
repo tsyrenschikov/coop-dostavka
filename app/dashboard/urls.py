eca2da368cc2b32528c34167b9086d5a12761f86
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2021-11-12 01:13:40 +0500

    user dashboard edit profile

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('edit_profile/<int:id>/', views.edit_profile, name='edit_profile'),
     path('my_rewards/', views.my_rewards,name='my_rewards'),
     path('my_wallet/', views.my_wallet,name='my_wallet'),
     path('my_wishlist/', views.my_wishlist,name='my_wishlist'),
-    path('my_addresses/', views.my_addresses, name='my_addresses'),
     ]
--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,7 +1,29 @@
+from django.contrib.auth import get_user_model
+User = get_user_model()
 from django.shortcuts import render
-from panel.models import offers
+from panel.models import *
+from django.http import HttpResponseRedirect
+from django.http import HttpResponseNotFound
+
+def edit_profile(request,id):
+    try:
+        user= User.objects.get(id=id)
+        user_id = int([i for i in str(request.path).split('/') if i][-1])
+        if request.method=="POST":
+            user.edit_email=request.POST.get("edit_email")
+            user.edit_phone = request.POST.get("edit_phone")
+            user.edit_address = request.POST.get("edit_address")
+            user.save()
+            return render(request, 'dashboard/dashboard_my_orders.html', { 'user':user, 'user_id':user_id })
+        else:
+            return render(request, 'dashboard/edit_profile.html', { 'user':user, 'user_id':user_id })
+    except User.DoesNotExist:
+        return render(request, 'dashboard/', {})
+
 
 def dashboard(request):
+    def edit_email(request,id):
+        return render(request, 'dashboard/dashboard_my_orders.html', {})
     return render(request, 'dashboard/dashboard_my_orders.html', {})
 
 def my_rewards(request):

c4ee6357c42ce63b5aae4e204168448c650ae649
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2021-11-23 22:05:04 +0500

    edit location

--- app/panel/models.py
+++ app/panel/models.py
@@ -319,6 +319,32 @@ class shalinsk(models.Model):
     def __str__(self):
         return self.name
 
+class zajkovskoe(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=True, verbose_name='Магазин')
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Название продукта')
+    image = models.ImageField(upload_to='zajkovskoe/%Y/%m/%d', blank=True, null=True, verbose_name='Изображение')
+    description = models.TextField(blank=True, verbose_name='Описания продукта')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    discount = models.DecimalField(max_digits=10, decimal_places=2, null=True, verbose_name='Скидка')
+    subcat = models.JSONField(default=list, null=True, blank=True)
+    subsubcat = models.JSONField(default=list, null=True, blank=True)
+    width = models.IntegerField(blank=True, null=True, verbose_name='Ширина')
+    height = models.IntegerField(blank=True, null=True, verbose_name='Высота')
+    length = models.IntegerField(blank=True, null=True, verbose_name='Длина')
+    fabricator = models.CharField(max_length=200, null=True, db_index=True, verbose_name='Производитель')
+    color = models.CharField(max_length=200, null=True, db_index=True, verbose_name='Цвет')
+    material = models.CharField(max_length=200, null=True, db_index=True, verbose_name='Материал')
+    status = models.BooleanField(default=True, verbose_name='Активный')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Зайковское сельпо'
+        verbose_name_plural = 'Зайковское сельпо'
+        index_together = (('id'),)
+
+    def __str__(self):
+        return self.name
+
 
 class offers(models.Model):
     area = models.ForeignKey(Area, on_delete=models.CASCADE, null=True, verbose_name='Территория')

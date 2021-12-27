429d2ab23b1d03584e8048139381780b30b606c7
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2021-12-27 21:22:25 +0500

    pokrovskoe

--- app/shop/urls.py
+++ app/shop/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('arti/product/<int:id>/', views.shop_arti_product, name='shop_arti_product'),
     path('arti/grid/', views.shop_arti_grid, name='shop_arti_grid'),
     path('arti/career/', views.shop_arti_career, name='shop_arti_career'),
-    path('arti/artiobschepit/', views.shop_artiobschepit, name='shop_artiobschepit')
+    path('arti/artiobschepit/', views.shop_artiobschepit, name='shop_artiobschepit'),
+    path('pokrovskoe/', views.shop_pokrovskoe, name='pokrovskoe'),
+    path('pokrovskoe/products/', views.shop_pokrovskoe_products, name='shop_pokrovskoe_products'),
+    path('pokrovskoe/product/<int:id>/', views.shop_pokrovskoe_product, name='shop_pokrovskoe_product'),
+    path('pokrovskoe/grid/', views.shop_pokrovskoe_grid, name='shop_pokrovskoe_grid'),
+    path('pokrovskoe/career/', views.shop_pokrovskoe_career, name='shop_pokrovskoe_career'),
     ]
--- app/shop/views.py
+++ app/shop/views.py
@@ -83,3 +83,57 @@ def shop_artiobschepit(request):
                 product=artiobschepit.objects.all().order_by('?')[:20]
                 return render(request, 'arti/artiobschepit/index.html', {'product':product,'local':local,'name':name})
 
+# Shop pokrovskoe
+def shop_pokrovskoe(request):
+    shop = Shop.objects.values_list('slug', flat=True).distinct()
+    areas = Area.objects.values_list('name', 'slug').distinct()
+    local = Locations.objects.values_list('name', 'slug').distinct()
+    address_str = str([i for i in str(request.path).split('/') if i][0])
+    for slug in shop:
+        for name_a, slug_a in areas:
+            if slug == address_str and slug == slug_a:
+                name = name_a
+                name_slug = eval(slug)
+                product = name_slug.objects.all().order_by('?')[:20]
+                return render(request, 'pokrovskoe/index.html', {'product': product, 'local': local, 'name': name})
+
+def shop_pokrovskoe_grid(request):
+    local=Locations.objects.values_list('name','slug').distinct()
+    shop= Shop.objects.values_list('slug', flat=True).distinct()
+    areas = Area.objects.values_list('name', 'slug').distinct()
+    address_str = str([i for i in str(request.path).split('/') if i][0])
+    for slug in shop:
+        for name_a, slug_a in areas:
+            if slug == address_str and slug == slug_a:
+                name = name_a
+                name_slug = eval(slug)
+                product=name_slug.objects.all().order_by('id')[::-1][:20]
+                return render(request, 'pokrovskoe/grid.html', {'product':product,'local':local,'name':name})
+
+#View products
+def shop_pokrovskoe_products(request):
+    local=Locations.objects.values_list('name','slug').distinct()
+    products= pokrovskoe.objects.all().order_by('?')[:20]
+    return render(request, 'pokrovskoe/products.html', {'products':products,'local':local})
+
+#View product
+def shop_pokrovskoe_product(request, id):
+    local=Locations.objects.values_list('name','slug').distinct()
+    product=pokrovskoe.objects.get(id=id)
+    products=pokrovskoe.objects.all().order_by('?')[:10]
+    shop = Shop.objects.values_list('slug', flat=True).distinct()
+    areas = Area.objects.values_list('name', 'slug').distinct()
+    address_str = str([i for i in str(request.path).split('/') if i][0])
+    for slug in shop:
+        for name_a, slug_a in areas:
+            if slug == address_str and slug == slug_a:
+                name = name_a
+                shop_name = slug
+                return render(request, 'pokrovskoe/product.html', {'product':product,'products':products,'shop_name':shop_name,'local':local,'name':name})
+
+def shop_pokrovskoe_career(reguest):
+    local=Locations.objects.values_list('name','slug').distinct()
+    users = User.objects.all()
+    categories = Category.objects.order_by('number')
+    return render(reguest, 'arti/career.html', {'users':users, 'categories' : categories,'local':local})
+

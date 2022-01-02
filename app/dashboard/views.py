60a81c081d46c9df8a2ad34457030ae11e4900e3
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2022-01-02 13:59:01 +0500

    select city dashboard

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -3,9 +3,11 @@ User = get_user_model()
 from django.shortcuts import render
 from panel.models import *
 from django.contrib.auth.hashers import  make_password
+from panel.models import*
 
 def edit_profile(request,id):
     try:
+        local = Locations.objects.values_list('name', 'slug').distinct()
         users_id = int([i for i in str(request.path).split('/') if i][-1])
         users = User.objects.get(id=id)
         if request.method=="POST":
@@ -19,30 +21,34 @@ def edit_profile(request,id):
                 if users.password:
                     users.password = make_password(users.password, hasher='default')
                     User.objects.filter(id=id).update(password=users.password)
-                    return render(request, "dashboard/edit_ok_profile.html", {'users': users,'users_id':users_id})
-            return render(request, 'dashboard/edit_ok_profile.html', { 'users':users, 'users_id':users_id })
+                    return render(request, "dashboard/edit_ok_profile.html", {'users': users,'users_id':users_id,'local': local})
+            return render(request, 'dashboard/edit_ok_profile.html', { 'users':users, 'users_id':users_id,'local': local })
         else:
-            return render(request, 'dashboard/edit_profile.html', { 'users':users, 'users_id':users_id })
+            return render(request, 'dashboard/edit_profile.html', { 'users':users, 'users_id':users_id,'local': local })
     except User.DoesNotExist:
         return render(request, 'dashboard/edit_profile.html', {})
 
 
 def dashboard(request):
+    local = Locations.objects.values_list('name', 'slug').distinct()
     zakaz=orders.objects.values('id','name_id', 'address_name_id__name','address_name_id__delivery_price','address_name_id__delivery_price_min','address_name_id__days','sum_order','total_order',
                                 'address')
-    local=Locations.objects.values('name')
     users=User.objects.all()
     return render(request, 'dashboard/dashboard_my_orders.html', {'zakaz':zakaz, 'users':users, 'local':local})
 
 def my_rewards(request):
-    return render(request, 'dashboard/dashboard_my_rewards.html', {})
+    local = Locations.objects.values_list('name', 'slug').distinct()
+    return render(request, 'dashboard/dashboard_my_rewards.html', {'local':local})
 
 def my_wallet(request):
+    local = Locations.objects.values_list('name', 'slug').distinct()
     offer= offers.objects.all()
-    return render(request, 'dashboard/dashboard_my_wallet.html', {'offer':offer})
+    return render(request, 'dashboard/dashboard_my_wallet.html', {'offer':offer, 'local': local})
 
 def my_wishlist(request):
-    return render(request, 'dashboard/dashboard_my_wishlist.html', {})
+    local = Locations.objects.values_list('name', 'slug').distinct()
+    return render(request, 'dashboard/dashboard_my_wishlist.html', {'local':local})
 
 def my_addresses(request):
-    return render(request, 'dashboard/dashboard_my_addresses.html', {})
+    local = Locations.objects.values_list('name', 'slug').distinct()
+    return render(request, 'dashboard/dashboard_my_addresses.html', {'local':local})

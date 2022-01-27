b2651cec26e38be20d9a2d6b4132c772b3692837
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2022-01-28 01:30:04 +0500

    calendar

--- www/static/file/panel/calendar/js/calendar.js
+++ www/static/file/panel/calendar/js/calendar.js
@@ -1,20 +1,25 @@
-$( "select" )
-    .change(function () {
-    var days = "";
-    $("select option:selected").each(function () {
-        days = $(this).attr('days');
-        if (days != "0" ) {
-            $('#datepicker-custom-classes').Zebra_DatePicker({
-                direction: true,
-                disabled_dates: ['* * * 0-6'],
-                enabled_dates: ['* * * 1,5'],
-            });
-        }
-        else
-        {
-            $('#datepicker-custom-classes').Zebra_DatePicker({
-                direction: false,
-            });
-        }
-    })
-})( jQuery );
+
+    $( "select" )
+        .change(function () {
+            var days = "";
+            $("select option:selected").each(function () {
+                days = $(this).attr('days') + " ";
+                if (days != "0" ) {
+                    var inputs = document.querySelectorAll('#datepicker');
+                    for (var i = 0;  i < inputs.length; i++) {
+                        inputs[i].value = '';
+                    };
+                    $('#datepicker').Zebra_DatePicker({
+                        direction: true,
+                        disabled_dates: ['* * * 0-6'],
+                        enabled_dates: ['* * * 1,5'],
+                    });
+                }
+                else
+                {
+                    $('#datepicker').Zebra_DatePicker({
+                        direction: false,
+                    });
+                }
+            })
+        })

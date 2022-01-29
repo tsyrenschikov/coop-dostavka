27d28b4bceb753c7c0fe94937d62ec5ea47008b9
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2022-01-29 11:30:14 +0500

    calendar

--- www/static/file/panel/calendar/js/calendar.js
+++ www/static/file/panel/calendar/js/calendar.js
@@ -1,25 +1,51 @@
+var shoppingCalendar = (function() {
 
-    $( "select" )
-        .change(function () {
-            var days = "";
-            $("select option:selected").each(function () {
-                days = $(this).attr('days') + " ";
-                if (days != "0" ) {
-                    var inputs = document.querySelectorAll('#datepicker');
-                    for (var i = 0;  i < inputs.length; i++) {
-                        inputs[i].value = '';
-                    };
-                    $('#datepicker').Zebra_DatePicker({
-                        direction: true,
-                        disabled_dates: ['* * * 0-6'],
-                        enabled_dates: ['* * * 1,5'],
-                    });
+  Calendar = [];
+  var obj = {};
+
+  // Calendar
+obj.Calendar = function () {
+  $("select")
+      .change(function () {
+        var days = "";
+        $("select option:selected").each(function () {
+          days += $(this).attr('days');
+        });
+        if (days != 0) {
+            var inputs = document.querySelectorAll('input[name=cal]');
+                for (var i = 0; i < inputs.length; i++) {
+                    inputs[i].value = '';
                 }
-                else
-                {
                     $('#datepicker').Zebra_DatePicker({
-                        direction: false,
+                            direction: true,
+                            disabled_dates: ['* * * 0-6'],
+                            enabled_dates: ['* * * 1,5'],
                     });
+        }
+        else
+        {
+          if (days == 0){
+              var inputs = document.querySelectorAll('input[name=cal]');
+                for (var i = 0; i < inputs.length; i++) {
+                    inputs[i].value = '';
                 }
-            })
-        })
+                $('#datepicker').Zebra_DatePicker({
+                    direction: true,
+                });
+          }
+          else{
+            delivery_e = Number(0)
+          $("delivery").text('бесплатно');
+          $("#delivery").attr({'value': delivery_e,})
+          }
+        }
+      })
+      .trigger("change");
+}
+
+
+  return obj;
+})();
+
+ $('.calendar').html(shoppingCalendar.Calendar());
+

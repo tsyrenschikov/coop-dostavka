f547ea88fa3c78325b166e8500c3a9280c0b1836
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-03-08 23:42:45 +0500

    panel orders view jquery change

--- www/static/file/panel/js/total_change.js
+++ www/static/file/panel/js/total_change.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
     var money=$("#nal").attr("value");
     var change=money-total;
     $("total_change").text(change);
-
+    var count_product = $("#count_p").attr("value");
+    var price_product = $("#price_p").attr("value");
+    var price = count_product * price_product;
+    $("total_price").text(price);
+    console.log(price)
 });

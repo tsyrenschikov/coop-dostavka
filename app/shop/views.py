d0ec1d44dd432c985918e41434b0cf022ea415ae
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-04-05 19:48:11 +0500

    rezh

--- app/shop/views.py
+++ app/shop/views.py
@@ -744,32 +744,29 @@ def cart_ok_pokrovskoe(request,ord):
 # Shop rezh
 def shop_rezh(request):
     shop = Shop.objects.values_list('slug', flat=True).distinct()
+    areas = Area.objects.values_list('name', 'slug').distinct()
     local = Locations.objects.values_list('name', 'slug').distinct()
     address_str = str([i for i in str(request.path).split('/') if i][0])
-    areas = Area.objects.values_list('name', 'slug').distinct()
+    categories = Category.objects.order_by('number')
     for slug in shop:
         for name_a, slug_a in areas:
             if slug == address_str and slug == slug_a:
                 name = name_a
                 name_slug = eval(slug)
-                products = name_slug.objects.all().order_by('name')
-                category_shop = Category.objects.values('name', 'subcat').order_by('number')
+                products = name_slug.objects.all().order_by('?')[:20]
+                new_products = name_slug.objects.all().order_by('id')[::-1][:20]
+                category_shop = Category.objects.values('name', 'subcat', 'image').order_by('number')
                 category_product = name_slug.objects.values_list('subcat', 'name', 'subsubcat').order_by('name')
-                list_category_product = {category['name']: [] for category in category_shop}
-                count_sidebar = len(list_category_product)
+                dict_category_product = {category['name']: [] for category in category_shop}
                 list_p = list(set([i for i, j, k in category_product]))
                 for category in category_shop:
                     for n in category['subcat']:
                         for i in list_p:
                             if i in n:
-                                list_category_product[category['name']].append(i)
-                category_product = dict(sorted(list_category_product.items()))
-                paginator = Paginator(products, 20)
-                page_number = request.GET.get('page')
-                page_obj = paginator.get_page(page_number)
-                return render(request, 'rezh/products.html',
-                              {'category_product': category_product, 'products': products,
-                               'page_obj': page_obj, 'name': name, 'local': local, 'address_str': address_str, 'count_sidebar': count_sidebar})
+                                dict_category_product[category['name']].append(i)
+                category_product = dict(sorted(dict_category_product.items()))
+                return render(request, 'rezh/products.html', {'products': products, 'new_products': new_products, 'category_product': category_product, 'categories': categories, 'local': local, 'name': name,
+                                                           'address_str': address_str})
 
 def sort_list_rezh(request,list):
     shop = Shop.objects.values_list('slug', flat=True).distinct()
@@ -891,7 +888,7 @@ def shop_rezh_career(reguest):
     local=Locations.objects.values_list('name','slug').distinct()
     users = User.objects.all()
     categories = Category.objects.order_by('number')
-    return render(reguest, 'bogdan/career.html', {'users':users, 'categories' : categories,'local':local})
+    return render(reguest, 'rezh/career.html', {'users':users, 'categories' : categories,'local':local})
 
 
 def cart_rezh(request):

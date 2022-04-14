bb44e0e0d922de302fae00d2dbe33355af1ff8eb
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2022-04-14 14:23:21 +0500

    chek panel

--- www/static/file/panel/js/checken.js
+++ www/static/file/panel/js/checken.js
@@ -1,11 +1,11 @@
 $(this).change(function(e) {
     var myArray = [];
     $("input:checkbox:checked").each(function() {
-        check=$(this).val()
+        check=$(this).val();
         myArray.push(check);
     });
     $("#check").attr({
         'value':myArray,
-    })
+    });
     console.log(myArray);
 });

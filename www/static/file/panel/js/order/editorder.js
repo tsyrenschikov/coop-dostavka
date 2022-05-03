2d4dc552f21b93d1b9841bd05d0831a099437b5d
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-05-03 20:51:31 +0500

    order edit count

--- www/static/file/panel/js/order/editorder.js
+++ www/static/file/panel/js/order/editorder.js
@@ -1,78 +1,94 @@
 var isEditing = false,
-	tempDataValue = "";
+    tempDataValue = "";
 
+$('#mytable tr').each(function () {
+    count = $(this).find("td").eq(2).html();
+    price = $(this).find("td").eq(3).html();
+    sum = ((parseFloat(count)) * (parseFloat(price)));
+    $(this).find('td').eq(4).text(sum.toFixed(2) + ' ' + 'р.');
+    console.log($(this).find("td").eq(2).html());
+
+});
 // Handles live/dynamic element events, i.e. for newly created edit buttons
-$(document).on('click', '.edit', function() {
-	var parentRow = $(this).closest('tr'),
-		tableBody = parentRow.closest('tbody'),
-		tdData = parentRow.children('td.data');
+$(document).on('click', '.edit', function () {
+    var parentRow = $(this).closest('tr'),
+        tableBody = parentRow.closest('tbody'),
+        tdData = parentRow.children('td.data');
 
-	if (isEditing) {
-		var
-			dataInput = tableBody.find('input[name="data"]'),
-			tdDataInput = dataInput.closest('td'),
-			currentEdit = tdDataInput.parent().find('td.edit');
+    if (isEditing) {
+        var
+            dataInput = tableBody.find('input[name="data"]'),
+            tdDataInput = dataInput.closest('td'),
+            currentEdit = tdDataInput.parent().find('td.edit');
 
-		if ($(this).is(currentEdit)) {
-			// Save new values as static html
-			var
-				tdDataValue = dataInput.prop('value');
+        if ($(this).is(currentEdit)) {
+            // Save new values as static html
+            var
+                tdDataValue = dataInput.prop('value');
+            tdDataInput.empty();
+            tdDataInput.html(tdDataValue);
 
+        } else {
+            // Restore previous html values
 
-			tdDataInput.empty();
-
-
-			tdDataInput.html(tdDataValue);
-		} else {
-			// Restore previous html values
-
-			tdDataInput.empty();
+            tdDataInput.empty();
 
 
-			tdDataInput.html(tempDataValue);
-		}
+            tdDataInput.html(tempDataValue);
+
+        }
 
 
-		// Display static row
-		currentEdit.html('<i class="fa fa-pencil" aria-hidden="true"></i>');
-		isEditing = false;
-	} else {
-		// Display editable input row
-		isEditing = true;
-		$(this).html('<i class="fa fa-floppy-o" aria-hidden="true"></i>');
+        // Display static row
+        currentEdit.html('<i class="fa fa-pencil" aria-hidden="true"></i>');
+        isEditing = false;
+    } else {
+        // Display editable input row
+        isEditing = true;
+        $(this).html('<i class="fa fa-floppy-o" aria-hidden="true"></i>');
 
-		var
-			tdDataValue = tdData.html();
+        var
+            tdDataValue = tdData.html();
 
-		// Save current html values for canceling an edit
+        // Save current html values for canceling an edit
 
-		tempDataValue = tdDataValue;
+        tempDataValue = tdDataValue;
 
-		// Remove existing html values
+        // Remove existing html values
 
-		tdData.empty();
+        tdData.empty();
 
-		// Create input forms
+        // Create input forms
 
-		tdData.html('<input type="text" name="data" value="' + tdDataValue + '">');
-	}
+        tdData.html('<input type="text" name="data" value="' + tdDataValue + '">');
+
+    }
+    $('#mytable tr').each(function () {
+        count = $(this).find("td").eq(2).html();
+        price = $(this).find("td").eq(3).html();
+        sum = ((parseFloat(count)) * (parseFloat(price)));
+        $(this).find('td').eq(4).text(sum.toFixed(2) + ' ' + 'р.');
+        console.log($(this).find("td").eq(2).html());
+
+    });
 });
 
+
 // Handles live/dynamic element events, i.e. for newly created trash buttons
-$(document).on('click', '.trash', function() {
-	// Turn off editing if row is current input
-	if (isEditing) {
-		var parentRow = $(this).closest('tr'),
-			tableBody = parentRow.closest('tbody'),
-			tdInput = tableBody.find('input').closest('td'),
-			currentEdit = tdInput.parent().find('td.edit'),
-			thisEdit = parentRow.find('td.edit');
+$(document).on('click', '.trash', function () {
+    // Turn off editing if row is current input
+    if (isEditing) {
+        var parentRow = $(this).closest('tr'),
+            tableBody = parentRow.closest('tbody'),
+            tdInput = tableBody.find('input').closest('td'),
+            currentEdit = tdInput.parent().find('td.edit'),
+            thisEdit = parentRow.find('td.edit');
 
-		if (thisEdit.is(thisEdit)) {
-			isEditing = false;
-		}
-	}
+        if (thisEdit.is(thisEdit)) {
+            isEditing = false;
+        }
+    }
 
-	// Remove selected row from table
-	$(this).closest('tr').remove();
+    // Remove selected row from table
+    $(this).closest('tr').remove();
 });
--- www/static/file/panel/js/order/total_change.js
+++ www/static/file/panel/js/order/total_change.js
@@ -1,12 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var total = $("#total_price").attr("value");
     var money = $("#nal").attr("value");
     var change = money - total;
     $("total_change").text(change);
-});
-$('#mytable tr').each(function() {
-    var count = $(this).find("td").eq(2).html();
-    var price = $(this).find("td").eq(3).html();
-    var  sum =((parseFloat(count)) * (parseFloat(price)));
-    $(this).find('td').eq(4).text(sum+' '+ 'р.');
 });

2d543b1fbf7f34467011c02e75b6373b7797324f
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2022-06-01 16:57:31 +0500

    signup NO

--- www/static/file/shop/contact/checkbocks.js
+++ www/static/file/shop/contact/checkbocks.js
@@ -1,15 +1,29 @@
-document.getElementById("my_captcha_form").addEventListener("submit",function(evt)
-  {
+document.getElementById("my_captcha_form").addEventListener("submit", function (evt) {
 
-  var response = grecaptcha.getResponse();
-  if(response.length == 0)
-  {
-    //reCaptcha not verified
-    alert("Пожалуйста, подтвердите, что вы не робот");
-    evt.preventDefault();
-    return false;
-  }
-  //captcha verified
-  //do the rest of your validations here
+    var response = grecaptcha.getResponse();
+    if (response.length == 0) {
+        //reCaptcha not verified
+        //alert("Пожалуйста, подтвердите, что вы не робот");
+        $('text').text('Пожалуйста, пройдите проверку!')
+        evt.preventDefault();
+        return false;
+    }
+    //captcha verified
+    //do the rest of your validations here
 
+});
+
+$("form").validate({
+    submitHandler: function (form) {
+        var response = grecaptcha.getResponse();
+        //recaptcha failed validation
+        if (response.length == '0') {
+            $('text').text('Пожалуйста, пройдите проверку!')
+            return false;
+        }
+        //recaptcha passed validation
+        else {
+            return true;
+        }
+    }
 });

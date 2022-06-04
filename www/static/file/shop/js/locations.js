1b5839f224097a4ede1d3d07848575c3bbf6565c
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-06-05 02:14:36 +0500

    locations OK

--- www/static/file/shop/js/locations.js
+++ www/static/file/shop/js/locations.js
@@ -34,19 +34,14 @@ $(document).ready(function () {
             minChars: 1,
             onSelect: function (suggestion) {
                 $('#selection').html('Вы выбрали: ' + suggestion.value);
-                var city_local;
                 var field = suggestion.value;
                 localStorage.location = field;
-                var city_local = suggestion.value;
-                $.each(nhlTeams, function (indexname, valuename){
-                    $.each(list_slug, function (indexlist, valuelist){
-                        console.log(valuelist)
-                        if (city_local === valuename && valuelist.length === 1) {
-                            window.location.href = 'https://coop-dostavka.ru/' + valuelist[indexlist],
+                $.each(list_name, function (indexname, valuename) {
+                    $.each(list_slug, function (indexlist, valuelist) {
+                        if ((suggestion.value === valuename) && (valuelist.length === 1) && (indexname === indexlist)) {
+                            window.location.href = 'https://coop-dostavka.ru/' + valuelist[0],
                                 window.location(window.location.href);
-                        }
-                        else
-                        {
+                        } else if ((suggestion.value === valuename) && (valuelist.length >= 1) && (indexname === indexlist)) {
                             window.location.href = 'https://coop-dostavka.ru/' + valuelist[0],
                                 window.location(window.location.href);
                         }
@@ -62,14 +57,13 @@ $(document).ready(function () {
             $('#select_local').attr({
                 'value': localStorage.location,
             });
-        }
-        /*else {
+        } else {
             $(window).load(function () {
                 if (window.location.href.indexOf('shop') == -1) {
-                    window.location.replace('https://coop-dostavka.ru/');
+                    window.location.replace('https://coop-dostavka.ru/'+'?shop');
                 }
             });
-        }*/
+        }
 
 
 // Initialize autocomplete with custom appendTo:

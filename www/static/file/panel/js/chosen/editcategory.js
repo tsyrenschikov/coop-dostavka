3ac89fcf84ff1a8020a875ff86595884cba4e7ff
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-06-12 00:53:28 +0500

    panel add_product chosen NO

--- www/static/file/panel/js/chosen/editcategory.js
+++ www/static/file/panel/js/chosen/editcategory.js
@@ -3,39 +3,43 @@ var
     subcategory = 0,
     subsubcategory = [];
 
-$('#tgdiv').fadeOut();
-
 $(".chosen").chosen({
     allow_single_deselect: true,
     no_results_text: "Совпадений не найдено: "
 
 });
 
+$('#tgdiv').fadeOut();
+
 $("select").change(function () {
     var
         count = 0;
     category = 0;
-    subcategory = 0;
     category = [];
+    subcategory = 0;
+    subsubcategory = 0;
 
     /*Считываем выбранный продукт при добавлении или редактировании продукта*/
     $(".cat option:selected").each(function () {
         count = $(this).attr('number');
+        console.log($(this).attr('number'))
     });
     subcategory = $('namesubcat' + count + '').attr('value');
+
+    /*Добавляем выбранную категорию с подкатегориями*/
+    subcategory = subcategory.replace(/'/g, '"');
+    subcategory = JSON.parse(subcategory);
     if (subcategory !== undefined) {
         $('#tgdiv').fadeIn();
     } else {
         $('#tgdiv').fadeOut();
     }
 
-    /*Добавляем выбранную категорию с подкатегориями*/
-    subcategory = subcategory.replace(/'/g, '"');
-    subcategory = JSON.parse(subcategory);
-    $('#selectsub').empty();
-    $.each(subcategory, function (i, v) {
+
+
+    /*$('#selectsub').empty();
+    /!*$.each(subcategory, function (i, v) {
         $("#selectsub").append($('<option name="subcat" value="' + v + '">' + v + '</option>'));
     });
-    $("#selectsub").trigger("chosen:updated");
-    console.log($(".chosen-select").trigger("chosen:updated"));
+    $('#selectsub').trigger("chosen:updated");*!/*/
 });

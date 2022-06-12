164d08bfc5de5fe825d0e1e058dd6d170ef907d4
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-06-12 20:56:20 +0500

    panel add_product chosen NO-OK

--- www/static/file/panel/js/chosen/editcategory.js
+++ www/static/file/panel/js/chosen/editcategory.js
@@ -1,18 +1,21 @@
 var
+    name = '',
     category = [],
     subcategory = 0,
     subsubcategory = [];
 
 $(".chosen").chosen({
     allow_single_deselect: true,
-    no_results_text: "Совпадений не найдено: "
-
+    no_results_text: "Совпадений не найдено: ",
+    search_contains: true,
+    display_selected_options: true
 });
 
-$("select").change(function () {
+$("select").on('change',function () {
     var
         count = 0;
     category = 0;
+    name = '';
     category = [];
     subcategory = 0;
     subsubcategory = 0;
@@ -20,10 +23,10 @@ $("select").change(function () {
     /*Считываем выбранный продукт при добавлении или редактировании продукта*/
     $(".cat option:selected").each(function () {
         count = $(this).attr('number');
+        name = $(this).text();
     });
-
     subcategory = $('namesubcat' + count + '').attr('value');
-
+    console.log(name)
     /*Добавляем выбранную категорию с подкатегориями*/
     subcategory = subcategory.replace(/'/g, '"');
     subcategory = JSON.parse(subcategory);
@@ -31,6 +34,6 @@ $("select").change(function () {
     $('#selectsub').empty();
     $('#selectsub').prepend("<option>--Выберите товарную группу--</option>");
     $.each(subcategory, function (i, v) {
-        $("#selectsub").append($("<option></option>", {number:i,value: v, text: v}));
+        $("#selectsub").append($("<option></option>", {value: v, text: v}));
     });
 });

50fce9a0d1b8c22b8112d3d11390421004b02f0f
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-06-14 01:43:03 +0500

    panel add_product chosen NO

--- www/static/file/panel/js/chosen/editcategory.js
+++ www/static/file/panel/js/chosen/editcategory.js
@@ -8,9 +8,8 @@ $(".chosen").chosen({
     allow_single_deselect: true,
     no_results_text: "Совпадений не найдено: ",
 });
+$("select").change(function () {
 
-
-$("select").chosen().change(function () {
     var
         count = 0;
     category = 0;
@@ -18,21 +17,21 @@ $("select").chosen().change(function () {
     category = [];
     subcategory = 0;
     subsubcategory = 0;
-
     /*Считываем выбранный продукт при добавлении или редактировании продукта*/
     $(".cat option:selected").each(function () {
         count = $(this).attr('number');
-    });
-    subcategory = $('namesubcat' + count + '').attr('value');
+        subcategory = $('namesubcat' + count + '').attr('value');
 
-    /*Добавляем выбранную категорию с подкатегориями*/
-    subcategory = subcategory.replace(/'/g, '"');
-    subcategory = JSON.parse(subcategory);
+        /*Добавляем выбранную категорию с подкатегориями*/
+        subcategory = subcategory.replace(/'/g, '"');
+        subcategory = JSON.parse(subcategory);
 
-    $('#selectsub').empty();
-    $.each(subcategory, function (i, v) {
-        var newrow = $('<option name="subcat" value="' + v + '">' + v + '</option>');
-        $('#selectsub').append(newrow);
-        $('#selectsub').trigger('chosen:updated');
+        $('#selectsub').empty();
+        $.each(subcategory, function (i, v) {
+            var newrow = $('<option name="subcat" value="' + v + '">' + v + '</option>');
+            $('#selectsub').append(newrow);
+            $('#selectsub').trigger('chosen:updated');
+        });
     });
-}).trigger('change');
+})
+    .trigger('change');

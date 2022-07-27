0895ff4824e20dac15f656d0d65a5f4871c81a88
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-07-28 00:34:43 +0500

    panel day location JS NO

--- www/static/file/panel/js/days/selected.js
+++ www/static/file/panel/js/days/selected.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $("#selected_name").on('change', function () {
         var str = "";
         $("#selected_name option:selected").each(function () {
-            str += $( this ).text().trigger("chosen:updated")
+            str += $( this ).text()
         });
         console.log(str)
     }).trigger('change');

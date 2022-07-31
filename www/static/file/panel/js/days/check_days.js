c20b2ff4073e521fffde2dc39f42ebc915c81d7c
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-07-31 19:42:25 +0500

    panel edit location NO

--- www/static/file/panel/js/days/check_days.js
+++ www/static/file/panel/js/days/check_days.js
@@ -1,13 +1,25 @@
 var
-    myArray = [];
+    myArray = [],
+    Array_days = [];
 
-$(this).change(function (e) {
-    myArray = 0;
-    myArray = [];
-    $("input:checkbox:checked").each(function () {
-        check = $(this).attr('number');
-        myArray.push(check);
+$(document).ready(function () {
+    Array_days = $('#array_days').val();
+    var array_str = Array_days.replace(/'/g, '');
+    array_str = JSON.parse(array_str);
+    $.each(array_str,function (i,v){
+        console.log(v)
+        $('#checkday').attr({
+            "number" : v,
+            "checked":"checked"
+        });
     });
-    $('#array').attr({'value' : myArray});
-    console.log(myArray)
+    $(this).change(function (e) {
+        myArray = 0;
+        myArray = [];
+        $("input:checkbox:checked").each(function () {
+            check = $(this).attr('number');
+            myArray.push(check);
+        });
+        $('#array').attr({'value': myArray});
+    });
 });

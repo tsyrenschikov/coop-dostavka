d15d3452cf3b420b8618354d8592aaa026904dd0
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-08-15 00:06:42 +0500

    panel category number

--- www/static/file/panel/js/category/category.js
+++ www/static/file/panel/js/category/category.js
@@ -2,18 +2,14 @@ $(document).ready(function () {
     var count_number = Number($('#count').val());
     $("input").on('change keydown paste input', function () {
         var number = Number($('#number').val());
+        console.log(number)
         if (number <= count_number) {
             $('msg_number').html("<span style='font-size: 14px; color: red'>Номер категории занят*</span>");
             $('msg_old').hide();
-            $('#button').hide();
         } else {
             $('msg_number').html("<span style='font-size: 14px; color: green'>Номер категории свободен*</span>");
             $('msg_old').hide();
-            $('#button').show();
         }
-        if (number === 0){
-          $('msg_old').show();
-          $('msg_number').html("<span style='font-size: 14px; color: red'></span>");
-        }
+
     });
 });

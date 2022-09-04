c736c555c09289ce5d6fc8cece6b81cf243b68c7
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-09-04 15:19:19 +0500

    panel add_product artikul js OK

--- www/static/file/panel/js/artikul_true.js
+++ www/static/file/panel/js/artikul_true.js
@@ -1,24 +1,40 @@
-$(document).ready(function (){
+$(document).ready(function () {
     var list_artikul = [];
-     $('#artikul td').each(function (i) {
-         var artikul_base = $(this).attr('value');
-         if ((artikul_base !== "None") && (artikul_base.length >0)) {
-             list_artikul.push(artikul_base);
-         }
-     });
-    $("#artikul").on('change keydown paste input', function () {
-        var artikul = parseInt($('#artikul').val());
-        console.log(artikul.length)
-        $.each(list_artikul, function (index, value){
-            if(artikul.length <=7){
-                $('message').html("<span style='font-size: 14px; color: red'>Не менее 8 символов</span>")
-            }
-            else if(artikul === value){
-                $('message').html("<span style='font-size: 14px; color: red'>Номер номенклатуры уже существует</span>")
-            }
-            else {
-                $('message').html("<span style='font-size: 14px; color: green'>Совпадений не найдено</span>")
-            }
-        })
+    $('button').hide();
+    $('message_button').html("<span style='font-size: 14px; color: red'>Проверьте номенклатурный код(артикул) *</span>")
+    $('#artikul td').each(function () {
+        var artikul_base = $(this).attr('value');
+        if ((artikul_base !== "None") && (artikul_base.length > 0)) {
+            list_artikul.push(artikul_base);
+        }
+    });
+    $("#artikul").bind('change keydown paste input', function () {
+        $(this).val($(this).val().toUpperCase());
+        var artikul_input = $(this).val();
+        if (artikul_input.length < 8) {
+            $('message').html("<span style='font-size: 14px; color: red'>Не менее 8 символов</span>")
+            $('button').hide();
+            $('message_button').html("<span style='font-size: 14px; color: red'>Проверьте номенклатурный код(артикул) *</span>")
+        }
+        if(artikul_input.length === 8) {
+            $.each(list_artikul, function (i, v) {
+                if (artikul_input === v) {
+                    $('message').html("<span style='font-size: 14px; color: red'>Номер номенклатуры занят</span>")
+                    $('button').hide();
+                    $('message_button').html("<span style='font-size: 14px; color: red'>Проверьте номенклатурный код(артикул) *</span>")
+                    return false
+                }
+                else {
+                    $('message').html("<span style='font-size: 14px; color: green'>Номер номенклатуры свободен</span>")
+                    $('button').show();
+                    $('message_button').html("")
+                }
+            });
+        }
+        if (artikul_input.length >8){
+            $('message').html("<span style='font-size: 14px; color: red'>Превышено кол-во символов</span>")
+            $('button').hide();
+            $('message_button').html("<span style='font-size: 14px; color: red'>Проверьте номенклатурный код(артикул) *</span>")
+        }
     });
 })

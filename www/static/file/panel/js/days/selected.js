19f25ea052f8fe409bf96b945ff51fcc639f0b0b
Tsyrenschikov <tsyrenschikov@gmail.com>
2022-11-20 18:31:10 +0500

    add_location html,css,js add

--- www/static/file/panel/js/days/selected.js
+++ www/static/file/panel/js/days/selected.js
@@ -1,13 +1,27 @@
-var
-    myArray = [];
+$(document).ready(function () {
+    var
+        myArray = [];
+    $('#days_time').hide()
+    $(this).change(function (e) {
+        myArray = 0;
+        myArray = [];
 
-$(this).change(function (e) {
-    myArray = 0;
-    myArray = [];
-    $("input:checkbox:checked").each(function () {
-        check = $(this).attr('number');
-        myArray.push(check);
+        $("input:checkbox:checked").each(function () {
+            check = $(this).attr('number');
+            myArray.push(check);
+            console.log(check)
+        });
+        $('#array').attr({'value': myArray});
     });
-    $('#array').attr({'value' : myArray});
-    console.log(myArray)
+    $('#days_dop').change(function() {
+        if($('#checkbox').is(':checked'))
+        {
+            $("input:checkbox:checked").each(function () {
+                $('#days_time').show()
+            });
+        }
+        else{
+            $('#days_time').hide()
+        }
+});
 });

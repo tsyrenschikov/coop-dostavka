a6fa3b67e461fec704aa950294b8c59feddd49cf
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2022-12-26 12:49:48 +0500

    update

--- www/static/file/panel/js/select/select_file.js
+++ www/static/file/panel/js/select/select_file.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
-    var href = '',
-        check_href = [],
-        check = '',
-        ost = 0;
     $('.ost').each(function (i, v) {
         href = $(v).find('.link').attr('value');
-        $(this).html('<a class="update" type="submit" href="' + href + '">Обновить</a>');
+        $(this).html('<a class="update" type="submit" href="' + href + '">Подтвердить</a>');
         check_href.push(href.replace(/[/update_file/]/g, ''))
     });
 });

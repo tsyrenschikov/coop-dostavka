26573dd8e87ddd9a0a6ab3c2d83b76a5c22689b3
Tsyrenschikov <tsyrenschikov@gmail.com>
2023-01-30 23:53:54 +0500

    edit jquery true artikul

--- www/static/file/panel/js/edit_artikul_true.js
+++ www/static/file/panel/js/edit_artikul_true.js
@@ -19,21 +19,25 @@ $(document).ready(function () {
             $.each(list_artikul, function (i, v) {
                 if ((artikul_input === v) && (art_product === v)) {
                     $('message').html("<span style='font-size: 14px; color: green'>Номер номенклатуры(артикул) совпадает с прежним значением</span>")
-                     $('message_button').html("")
+                    $('message_button').html("")
+                    $('#disbut').show()
                     return false
                 } else if ((artikul_input === v) && (art_product !== v)) {
                     $('message').html("<span style='font-size: 14px; color: red'>Номер номенклатуры(артикул) занят. Прежний артикул продукта " + art_product + "</span>")
-                     $('message_button').html("")
+                    $('message_button').html("")
+                    $('#disbut').hide()
                     return false
                 } else {
                     $('message').html("<span style='font-size: 14px; color: green'>Номер номенклатуры(артикул) свободен</span>")
-                     $('message_button').html("")
+                    $('message_button').html("")
+                    $('#disbut').show()
                 }
             });
         }
         if (artikul_input.length > 8) {
             $('message').html("<span style='font-size: 14px; color: red'>Превышено кол-во символов</span>")
             $('message_button').html("<span style='font-size: 14px; color: red'>Превышено кол-во символов в поле Артикул</span>")
+            $('#disbut').hide()
         }
     });
 })

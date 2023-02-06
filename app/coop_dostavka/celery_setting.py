ba17cc311696768ca04734cd382797abfa4d54b8
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2023-02-06 14:48:19 +0500

    celery

--- app/coop_dostavka/celery_setting.py
+++ app/coop_dostavka/celery_setting.py
@@ -1,8 +1,8 @@
 # from __future__ import absolute_import
-# import os
-# from celery import Celery
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coop_dostavka.settings')
-# app = Celery()
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-# app.autodiscover_tasks()
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coop_dostavka.settings')
+app = Celery()
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()

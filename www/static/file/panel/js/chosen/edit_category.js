2f3506bef4a7f6abc3273f0965a249a8771974e6
tsyrenschikov <77172321+tsyrenschikov@users.noreply.github.com>
2023-03-10 13:38:55 +0500

    edit category NO

--- www/static/file/panel/js/chosen/edit_category.js
+++ www/static/file/panel/js/chosen/edit_category.js
@@ -1,25 +1,29 @@
 $(document).ready(function () {
     var array = [];
-    var select = $(".edit-multiselect")
+    var select = $(".edit-multiselect");
     /*Элементы из базы*/
     $('table tbody td').each(function () {
         array.push($(this).attr('value'));
     });
-    $.each(array, function (i,v){
-        $('#choose').append('<input id="'+ i +'" name="subcat" value="' + v + '">');
-    })
     select.chosen()
-
-    /*Считать выбранные рание подкатегории*/
     $.each(array, function (i, v) {
+        $('#choose').append('<input id="' + i + '" name="subcat" value="' + v + '">');
         $('select option:contains(' + v + ')').val(v).prop('selected', true).trigger("chosen:updated")
-    });
+    })
+
     /*Выбранные элементы*/
     select.chosen().change(function (e) {
         var sel = $('select').val();
-        console.log(sel.indexOf('Крупы')!=-1)
+        $.each(array, function (i, v) {
+            $('#' + i + '').remove();
+        });
         $.each(sel, function (i, v) {
+            $('#choose').append('<input name="subcat" value="' + v + '">');
             $('select option:contains(' + v + ')').val(v).prop('selected', true).trigger("chosen:updated");
+            var subcat = $(this).val(v)
+            if (sel.indexOf(v) !== subcat) {
+                $(this).val(v).remove();
+            }
         });
     });
 })

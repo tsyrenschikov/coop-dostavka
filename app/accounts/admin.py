1ebcfbfc2fe4479b536e37055cd219e4a56322c6
Tsyrenschikov <tsyrenschikov@gmail.com>
2023-07-22 21:09:37 +0300

    new netbook

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-#DDFD
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
